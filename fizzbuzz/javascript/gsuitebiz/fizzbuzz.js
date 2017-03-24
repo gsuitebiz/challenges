f0ebcc270382d032709d4149880acd8ae69fa739
Crystal Milliken <MagicSauce@Crystals-MacBook-Pro.local>
2017-03-24 14:16:33 -0400

    corrected javascript

--- fizzbuzz/javascript/gsuitebiz/fizzbuzz.js
+++ fizzbuzz/javascript/gsuitebiz/fizzbuzz.js
@@ -4,17 +4,22 @@ function writeNumber(arrayLength){
 		var y = 5;
 		var remainder3 = i%x;
 		var remainder5 = i%y;
+		var arrayOfNumbers = new Array();
 
 		if(remainder3 == 0 && remainder5 == 0){
 			console.log("fizzbuzz");
+			arrayOfNumbers.push("fizzbuzz");
 		}else if(remainder3 == 0){
 			console.log("fizz");
+			arrayOfNumbers.push("fizz");
 		}else if(remainder5 == 0){
 			console.log("buzz")
+			arrayOfNumbers.push("buzz");
 		}else{
 			console.log(i);
+			arrayOfNumbers.push(i);
 		}
 		
 	}
-
+console.log(arrayOfNumbers);
 }
